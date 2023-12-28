bef1086d04710c1292f3acf839c2f715ff4bc2ce
Juanjo Cano <juanjocanoferreira@gmail.com>
2023-12-28 14:16:10 +0100

    profile-card_createdç

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+const toggleColors = document.getElementById('toggle-colors');
+const rootStyles = document.documentElement.style;
+
+toggleTheme.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark');
+    if(toggleIcon.src.includes('moon.svg')){
+        toggleIcon.src= 'assets/icons/sun.svg'; 
+        toggleText.src= 'Light mode';
+    } else{
+        toggleIcon.src= 'assets/icons/moon.svg'; 
+        toggleText.src= 'Dark mode';
+    }
+});
+
+toggleColors.addEventListener('click', (e)=>{
+    rootStyles.setProperty('--primary-color', e.target.dataset.colors);
+});
